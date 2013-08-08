f9af4276ee6049e530c7606c0950502b97973c89
eniirane <eniirane@192.168.1.33>
2013-08-08 16:47:41 +0300

    Off by one -bugi korjattu.

--- DueRL/src/World/Environment.java
+++ DueRL/src/World/Environment.java
@@ -15,9 +15,7 @@ public class Environment {
     private String floorDescription    = "You see just smooth floor.";
 
     private int worldSize = 20;
-    
-    private int[][] world;
-    
+        
     public Environment() {
         buildWorld();
         populate();
@@ -27,23 +25,32 @@ public class Environment {
         /* 
          * Building world geometry.  0 is interpreted as floor, 1 as wall.
          */
+        int[][] world = new int[worldSize][worldSize];
+    
         for (int i=0;i<worldSize;i++) {
-            for (int j=0;i<worldSize;j++) {
+            for (int j=0;j<worldSize;j++) {
                 world[i][j] = 0;
             }
         }
-
+        
         // Setting walls to outer limits of the world.
         for (int i=0;i<worldSize;i++) {
             world[i][0]  = 1;
-            world[i][worldSize] = 1;
+            world[i][worldSize-1] = 1;
             world[0][i]  = 1;
-            world[worldSize][i] = 1;
+            world[worldSize-1][i] = 1;
+        }
+        
+        for (int[] row : world) {
+            for (int i : row) {
+                System.out.print(i);
+            }
+            System.out.println();
         }
     }
     
     private void populate() {
-        Creature protagonist = new Creature(worldSize/4, worldSize/4, "protagonist");
-        Creature antagonist = new Creature((worldSize/4)*3, (worldSize/4)*3, "antagonist");
+        Creature protagonist = new Creature(2, 2, "Protagonist");
+        Creature antagonist = new Creature(3, 3, "Antagonist");
     }
 }
